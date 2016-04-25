06a10114bf0439a4ade5402209dbe243dbc53a05
Cory Fauver <cofauver@gmail.com>
2016-04-25 11:43:24 -0700

    rspec terminal command fixed changed test names

--- username.rb
+++ username.rb
@@ -1,5 +1,5 @@
 # Make sure to run the tests in your /spec folder
-# Run `rspec /spec/username_spec.rb` to get started.
+# Run `rspec spec/username_spec.rb` to get started.
 
 def format_name(first, last)
   nil
