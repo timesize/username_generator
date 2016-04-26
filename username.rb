c252e1b70b640e8e023346de0f009d28907fb697
Nathan L <adamnate@gmail.com>
2016-04-26 14:17:05 -0700

    improved answers

--- username.rb
+++ username.rb
@@ -2,13 +2,50 @@
 # Run `rspec spec/username_spec.rb` to get started.
 
 def format_name(first, last)
-  nil
+  return nil if first.empty? || last.empty?
+  first.gsub! (/[^a-z]/i, "")
+  last.gsub! (/[^a-z]/i, "")
+  ((first.split(" ").join[0]<<last).split(" ").join.downcase)
+
 end
 
-def format_year
-  nil
+def format_year year
+  year_string = year.to_s
+  if year.length == 4
+    year_string[2,3]
+  end
 end
 
-def build_username
-  nil
+def build_username (first_name, last_name, year)
+  "#{format_name(first_name,last_name)}#{format_year(year)}"
+end
+
+#level2
+
+def check_privilege(type = 0)
+user_types = ["user", "seller", "manager", "admin"]
+type = 3 if type > 3
+user_types[type.floor]
+
+end
+
+def user_type_prefix(lvl)
+  prefix = check_privilege(lvl)
+  unless prefix == "user" then prefix << "-" else "" end
+
+
+end
+
+def build_username arg
+
+  arg.prepend"user_type"
+end
+
+#level 3
+
+def generate_username (first_name,last_name,birth_year,privilege_level)
+  _unique_
+
+  return _unique_
+
 end
--- warmup.rb
+++ warmup.rb
@@ -1,17 +1,21 @@
 # Run `rspec spec/the_warmup_spec.rb` to get started.
 
 def say_hello
-  "hi"
+  "hello"
 end
 
 def scream(message)
-  message
+  message.upcase<<"!"
 end
 
-def first_char
-  "z"
+def first_char(arg)
+  (arg)[0].downcase
 end
 
-def polly_wanna
-  "crackercrackercracker"
+def polly_wanna (arg)
+  (arg)<<(arg)<<(arg)
+end
+
+def after_you
+    "no, after you"
 end
